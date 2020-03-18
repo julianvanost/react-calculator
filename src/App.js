3b17a9410ff63bf8a18a75e937cd3eabf7b9e7b7
Julian <Van Ost>
2020-03-18 13:22:40 -0700

    add app.js logic

--- src/App.js
+++ src/App.js
@@ -1,29 +1,87 @@
-import React, { Component } from 'react'
-import NumberButton from './components/NumberButton/NumberButton.js'
+import React from 'react'
+// import Math from "./components/Math.js";
+import NumberButton from "./components/NumberButton/index.js";
+
+const numOne = 0
+const numTwo = 0
+const operator = ''
+const result = 0 || ''
 
 class App extends Component {
-
   state = {
-    count: 0
+    numOne: '',
+    numTwo: '',
+    operator: '',
+    result: ''
   }
 
-  handleIncrementCount = value => {
-    this.setState({ count: this.state.count + value })
+  handleNumOne = value => {
+    this.setState({ numOne: this.state.numOne + value })
   }
+  handlenumTwo = value => {
+    this.setState({ numTwo: this.state.numTwo + value })
+  }
+  handleOperator = value => {
+    this.setState({ operator: this.state.operator + value })
+  }
+  handleNumOne = value => {
+    this.setState({ numOne: this.state.numOne + value })
+  }
+  handleEnterNumber = value => {
+    if (this.state.numOne === '' && this.state.numTwo === '') {
+      this.setState = numOne
+    }
+    if (this.state.numOne != '' && this.state.operator = '') {
+      handleNumOne(value)
+    }
+
+  }
+
+  handleEnteredNumbers = value => {
+
+  }
+
 
   render() {
     return (
       <div>
         <h1>Count: {this.state.count}</h1>
         <NumberButton
-          number={5}
-          handleIncrementCount={this.handleIncrementCount} />
+          text={'1'}
+          handleNumOne={this.handleNumOne} />
         <NumberButton
-          number={7}
-          handleIncrementCount={this.handleIncrementCount} />
+          text={'2'}
+          handleNumOne={this.handleNumOne} />
         <NumberButton
-          number={12}
-          handleIncrementCount={this.handleIncrementCount} />
+          text={'3'}
+          handleNumOne={this.handleNumOne} />
+        <NumberButton
+          text={'4'}
+          handleNumOne={this.handleNumOne} />
+        <NumberButton
+          text={'5'}
+          handleNumOne={this.handleNumOne}
+        />
+        <NumberButton
+          text={'6'}
+          handleNumOne={this.handleNumOne}
+        />
+        <NumberButton
+          text={'7'}
+          handleNumOne={this.handleNumOne}
+        />
+        <NumberButton
+          text={'8'}
+          handleNumOne={this.handleNumOne}
+        />
+        <NumberButton
+          text={'9'}
+          handleNumOne={this.handleNumOne}
+        />
+        <NumberButton
+          text={'0'}
+          handleNumOne={this.handleNumOne}
+        />
       </div>
     )
   }
